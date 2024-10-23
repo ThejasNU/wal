49df9e9a68bddec524ff1812ec8be8011359e82c
ThejasNU <thejasnu@gmail.com>
2024-10-24 00:15:01 +0530

    add wal initialisation

--- utils/helpers.go
+++ utils/helpers.go
@@ -1,2 +1,46 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	SyncInterval  = 200 * time.Millisecond
+	SegmentPrefix = "segment-"
+)
+
+// finds last segment id in the list of files
+func FindLastSegmentId(files []string) (uint, error) {
+	var lastSegmentId uint = 0
+
+	for _, file := range files {
+		_, fileName := filepath.Split(file)
+		curSegmentId, err := strconv.ParseUint(strings.TrimPrefix(fileName, SegmentPrefix),10,0)
+		if err != nil {
+			return uint(curSegmentId), err
+		}
+
+		if uint(curSegmentId) > lastSegmentId {
+			lastSegmentId = uint(curSegmentId)
+		}
+	}
+
+	return lastSegmentId, nil
+}
+
+// creates new segment file with given id in provided directory and returns it
+func CreateSegmentFile(directory string, segmentId int) (*os.File, error) {
+	filePath := filepath.Join(directory, fmt.Sprintf("%s%d", SegmentPrefix, segmentId))
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
--- wal.go
+++ wal.go
@@ -3,9 +3,15 @@ package wal
 import (
 	"bufio"
 	"context"
+	"fmt"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"sync"
 	"time"
+
+	"github.com/ThejasNU/wal/utils"
 )
 
 type WAL struct {
@@ -42,4 +48,102 @@ type WAL struct {
 	// to control and manage goroutines
 	ctx    context.Context
 	cancel context.CancelFunc
+}
+
+func GetWAL(directory string, enableFsync bool, maxFileSize uint64, maxSegments uint) (*WAL, error) {
+	// create directory if it does not exist
+	if err := os.MkdirAll(directory, 0755); err != nil {
+		return nil, err
+	}
+
+	// get list of all wal segment files present
+	files, err := filepath.Glob(filepath.Join(directory, fmt.Sprintf("%s*", utils.SegmentPrefix)))
+	if err != nil {
+		return nil, err
+	}
+
+	var lastSegmentId uint = 0
+	if len(files) > 0 {
+		lastSegmentId, err = utils.FindLastSegmentId(files)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// open last segment file and seek to it's end
+	lastSegmentFilePath := filepath.Join(directory, fmt.Sprintf("%s%d", utils.SegmentPrefix, lastSegmentId))
+	lastSegmentFile, err := os.OpenFile(lastSegmentFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = lastSegmentFile.Seek(0, io.SeekEnd); err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wal := &WAL{
+		directory:           directory,
+		currentSegment:      lastSegmentFile,
+		lastSequenceNumber:  0,
+		bufferWriter:        bufio.NewWriter(lastSegmentFile),
+		flushTimer:          time.NewTimer(utils.SyncInterval),
+		shouldFsync:         enableFsync,
+		maxSegmentSize:      maxFileSize,
+		maxSegmentsNumber:   maxSegments,
+		currentSegmentIndex: lastSegmentId,
+		ctx:                 ctx,
+		cancel:              cancel,
+	}
+
+	// TODO: update LSN
+
+	go wal.keepSyncing()
+
+	return wal, nil
+}
+
+// keeps checking till timer runs out and flushes data to file
+// in case of parent function exiting, this function is also terminated
+func (wal *WAL) keepSyncing() {
+	for {
+		select {
+		case <-wal.flushTimer.C:
+			wal.lock.Lock()
+			err := wal.Flush()
+			wal.lock.Unlock()
+
+			if err != nil {
+				log.Printf("Error while flushing WAL: %v", err)
+			}
+
+		case <-wal.ctx.Done():
+			return
+		}
+	}
+}
+
+// writes in-memory buffer to the segment file in file system
+// if fsync is opted, syncs file to the disk
+func (wal *WAL) Flush() error {
+	if err := wal.bufferWriter.Flush(); err != nil {
+		return err
+	}
+
+	if wal.shouldFsync {
+		if err := wal.currentSegment.Sync(); err != nil {
+			return err
+		}
+	}
+
+	// reset the timer, since we just synced
+	wal.resetTimer()
+
+	return nil
+}
+
+// resets the synchronization timer
+func (wal *WAL) resetTimer() {
+	wal.flushTimer.Reset(utils.SyncInterval)
 }
